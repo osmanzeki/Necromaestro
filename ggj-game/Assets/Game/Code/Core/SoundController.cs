2fcfdb1004f1ab27b12a196f16e4d6fb8b0fd70d
Osman Zeki <ozeki@turbulent.ca>
2016-01-31 14:20:01 -0500

    Play alls sounds and change volumes on event triggers

--- ggj-game/Assets/Game/Code/Core/SoundController.cs
+++ ggj-game/Assets/Game/Code/Core/SoundController.cs
@@ -25,11 +25,14 @@ public class SoundController : MonoBehaviour {
         FxTams2,
         FxMelodic1,
         FxWubBassSlow,
+		Count
     }
 
 	public enum MusicType {
 		MusicBaseline
 	}
+
+	private uint[] activeSounds = new uint[(int)SfxType.Count];
 
     protected void Awake() {
         instance = this;
@@ -44,13 +47,43 @@ public class SoundController : MonoBehaviour {
 			.DOFade(1.0f, 3f)
 			.SetEase(Ease.InSine);
 
+		// Start all sfx in the background so they stay on beat
+		sounds[(int)SfxType.FxCreeper1].volume = 0;
+		sounds[(int)SfxType.FxCreeper2].volume = 0;
+		sounds[(int)SfxType.FxTams1].volume = 0;
+		sounds[(int)SfxType.FxKickSlow].volume = 0;
+		sounds[(int)SfxType.FxDistortedHat].volume = 0;
+		sounds[(int)SfxType.FxWeirdDrummish].volume = 0;
+		sounds[(int)SfxType.FxKickBassFast].volume = 0;
+		sounds[(int)SfxType.FxMetal1].volume = 0;
+		sounds[(int)SfxType.FxMelodicDistorted1].volume = 0;
+		sounds[(int)SfxType.FxMelodic2].volume = 0;
+		sounds[(int)SfxType.FxTams2].volume = 0;
+		sounds[(int)SfxType.FxMelodic1].volume = 0;
+		sounds[(int)SfxType.FxWubBassSlow].volume = 0;
+
+		sounds[(int)SfxType.FxCreeper1].Play();
+		sounds[(int)SfxType.FxCreeper2].Play();
+		sounds[(int)SfxType.FxTams1].Play();
+		sounds[(int)SfxType.FxKickSlow].Play();
+		sounds[(int)SfxType.FxDistortedHat].Play();
+		sounds[(int)SfxType.FxWeirdDrummish].Play();
+		sounds[(int)SfxType.FxKickBassFast].Play();
+		sounds[(int)SfxType.FxMetal1].Play();
+		sounds[(int)SfxType.FxMelodicDistorted1].Play();
+		sounds[(int)SfxType.FxMelodic2].Play();
+		sounds[(int)SfxType.FxTams2].Play();
+		sounds[(int)SfxType.FxMelodic1].Play();
+		sounds[(int)SfxType.FxWubBassSlow].Play();
     }
 
-	public void PlayMusic (MusicType music) {
-		this.music[(int)music].Play();
+    public void PlaySfx (SfxType sfx) {
+		activeSounds[(int)sfx] += 1;
+		sounds[(int)sfx].volume = 1.0f;
+    }
+
+	public void StopSfx (SfxType sfx) {
+		activeSounds[(int)sfx] -= 1;
+		sounds[(int)sfx].volume = 0f;
 	}
-
-    public void PlaySfx (SfxType Sfx) {
-        this.sounds[(int)Sfx].Play();
-    }
 }
