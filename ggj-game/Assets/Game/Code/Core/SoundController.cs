98db69603f112e0d18bc8bad37e71ad8487eb3d0
Osman Zeki <ozeki@turbulent.ca>
2016-01-31 13:49:22 -0500

    Play baseline background music and tween its volume for 3 seconds.

--- ggj-game/Assets/Game/Code/Core/SoundController.cs
+++ ggj-game/Assets/Game/Code/Core/SoundController.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using DG.Tweening;
 
 public class SoundController : MonoBehaviour {
 
     public static SoundController instance;
 
     public AudioSource[] sounds;
+	public AudioSource[] music;
+
+	private AudioSource baselineMusic;
 
     public enum SfxType {
         FxCreeper1,
@@ -20,15 +24,33 @@ public class SoundController : MonoBehaviour {
         FxMelodic2,
         FxTams2,
         FxMelodic1,
-        FxWubBassSlow
+        FxWubBassSlow,
     }
+
+	public enum MusicType {
+		MusicBaseline
+	}
 
     protected void Awake() {
         instance = this;
+
+		// Play baseline music
+		baselineMusic = this.music[(int)MusicType.MusicBaseline];
+		baselineMusic.volume = 0f;
+		baselineMusic.Play();
+
+		// Tween volume
+		baselineMusic
+			.DOFade(1.0f, 3f)
+			.SetEase(Ease.InSine);
+
     }
 
+	public void PlayMusic (MusicType music) {
+		this.music[(int)music].Play();
+	}
 
     public void PlaySfx (SfxType Sfx) {
-        sounds[(int)Sfx].Play();
+        this.sounds[(int)Sfx].Play();
     }
 }
